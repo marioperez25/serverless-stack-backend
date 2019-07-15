f287a4881894104e3fe738833b5c45e98b12913f
unknown <mariofph@gmail.com.com>
2019-07-14 22:29:40 -0600

    Backend Deployed to AWS

--- handler.js
+++ handler.js
@@ -1,14 +1,19 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
+export const hello = async(event, context) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
+        }),
+        catch (e) {
+            console.log(e);
+            callback(null, failure({ status: false }));
+        }
+
+    };
 };
 
 const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
+    setTimeout(() => {
+        resolve(`${rest.copy} (with a delay)`);
+    }, time * 1000)
 );
